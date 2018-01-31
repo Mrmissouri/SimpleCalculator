390d1d57899223ba4822c3ffc5b940f21a8012d5
mrmissouri <dwayne.missouri@gmail.com>
2018-01-30 23:38:56 -0600

    First commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number to be added:");
+
+            int numOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number to be added:");
+
+            int numTwo = int.Parse(Console.ReadLine());
+
+            int answer = numOne + numTwo;
+
+            Console.WriteLine("The answer is: " + answer);
+
+            Console.ReadLine();
+        }
+    }
+}
